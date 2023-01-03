67143d19705e66a2f61392d7e11bf18b5e778df8
Prajwal Anil Mandlik <prajwalmandlik7@gmail.com>
2023-01-03 22:29:32 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Personal portfolio website
+
+This is my personal portfolio website 
+It contains my educational qualifications, my skills in various fields like technical skills and soft skills. I have also mention my contact details, this is a short and crips website to know my capabilities, interest and what kind of person I am
---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./skills.css";
+import Frontend from './Frontend';
+import Backend from './Backend';
+
+const Skills = () => {
+  return (
+    <section className="skills section" id="skills">
+        <h2 className="section_title">Skills</h2>
+        <span className="section_subtitle">My technical level</span>
+
+        <div className="skills__container container grid">
+            <Frontend />
+
+            <Backend />
+        </div>
+    </section>
+  )
+}
+
+export default Skills
