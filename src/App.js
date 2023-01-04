1590323432d1ed5abc5972ab8a2a6735da19fad3
Prajwal Anil Mandlik <prajwalmandlik7@gmail.com>
2023-01-04 19:59:28 +0530

    new component
    contact component add to website

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from "./components/about/About";
 import Skills from "./components/skills/Skills";
 import Services from "./components/services/Services";
 import Qualification from "./components/qualification/Qualification";
+import Content from "./components/contact/Contact";
 
 
 const App = () => {
@@ -19,6 +20,7 @@ const App = () => {
       <Skills />
       <Services />
       <Qualification />
+      <Content />
     </main>
     </>
   )
