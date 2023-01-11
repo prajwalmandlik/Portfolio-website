353640d2887a451c597f919d2a25a446b0ae0d80
Prajwal Anil Mandlik <prajwalmandlik7@gmail.com>
2023-01-11 13:43:15 +0530

    project component added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Qualification from "./components/qualification/Qualification";
 import Content from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 import ScrollUp from "./components/scrollUp/Scrollup";
+import Work from "./components/work/Work";
 
 
 const App = () => {
@@ -22,6 +23,7 @@ const App = () => {
       <Skills />
       <Services />
       <Qualification />
+      <Work />
       <Content />
     </main>
 
---
+++ src/components/work/WorksItems.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const WorksItems = ({item}) => {
+  return (
+    <div>
+      <div className="work__card" key={item.id}>
+        <img src={item.image} alt="" className='work__img' />
+        <h3 className="work__title">{item.title}</h3>
+        <a href="#a" className='work__button'>
+            Demo <i className="bx bx-right-arrow-alt work__button-icon"></i>
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default WorksItems
