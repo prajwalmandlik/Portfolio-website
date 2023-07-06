833de4b6e31cd5f60f2f9fed772f067464553832
Prajwal Anil Mandlik <prajwalmandlik7@gmail.com>
2023-07-06 17:09:13 +0530

    ADD - vercel analytics

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import { inject } from '@vercel/analytics/*';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+inject();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
